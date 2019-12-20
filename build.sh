356b445365eeb21595bd17badee284351092c3a9
juuso <juuso@vilmunen.net>
2019-12-20 14:00:09 +0200

    update

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ export BUNDLE_PATH=.vendor
 bundle install --path .vendor/bundle
 
 GEM_PATH=${GEM_PATH}:.vendor bundle exec jekyll build
-GEM_PATH=${GEM_PATH}:.vendor ./.vendor/bundle/ruby/2.5.0/bin/htmlproofer --help
-GEM_PATH=${GEM_PATH}:.vendor ./.vendor/bundle/ruby/2.5.0/bin/htmlproofer ./_site --allow-hash-href --check-favicon --check-html --disable-external
+#GEM_PATH=${GEM_PATH}:.vendor ./.vendor/bundle/ruby/2.5.0/bin/htmlproofer --help
+#GEM_PATH=${GEM_PATH}:.vendor ./.vendor/bundle/ruby/2.5.0/bin/htmlproofer ./_site --allow-hash-href --check-favicon --check-html --disable-external
 GEM_PATH=${GEM_PATH}:.vendor bundle exec jekyll $*
